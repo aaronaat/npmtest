d0206768a4f49554e43c7abc588b156eaaa62b9d
Guest User <Guest@epicodus-5.local>
2019-03-21 10:43:37 -0700

    test

---
+++ src/main.js
@@ -0,0 +1,12 @@
+// import { } from ''./game';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+import $ from 'jquery';
+
+$(document).ready(function() {
+  $('#ping-pong-form').submit(function(event) {
+    event.preventDefault();
+
+    });
+  });
